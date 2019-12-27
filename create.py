fd9bd7a35d0430bb7180cbf9fa09371449edcd24
Michal Kruszewski <kruszewski2@gmail.com>
2019-12-27 15:57:37 +0100

    create.py refactored

--- create.py
+++ create.py
@@ -1,7 +1,7 @@
 import sys
 import os
 from github import Github
-from selenium import webdriver
+# from selenium import webdriver
 import Credentials
 
 #browser = webdriver.Firefox()
@@ -22,12 +22,42 @@ import Credentials
 
 
 def create():
-    project_language = "python" # str(sys.argv[1])
-    project_name = "project" # str(sys.argv[2])
-    os.makedirs("{0}\\{1}\\{2}".format(Credentials.path, project_language, project_name))
-    user = Github(Credentials.username, Credentials.password).get_user()
-    repo = user.create_repo("{0}-{1}".format(project_language, project_name))
-    print("Repository {0}-{1} created successfully".format(project_language, project_name))
+    project_language = str(sys.argv[1])
+    project_name = str(sys.argv[2])
+    access_mode = get_access_mode()
+
+    create_repo_directory(project_language=project_language, project_name=project_name)
+    create_repository(project_language=project_language, project_name=project_name, access_mode=access_mode)
+
+
+def get_access_mode():
+    try:
+        access_mode = bool(sys.argv[3].capitalize())
+        if not access_mode:
+            raise Exception("wrong 3rd argument")
+    except IndexError:
+        access_mode = False
+    except Exception as error:
+        print("An error occured: {0}".format(error))
+    return access_mode
+
+
+def create_repo_directory(project_language, project_name):
+    try:
+        directory = "{0}\\{1}\\{2}".format(Credentials.path, project_language, project_name)
+        os.makedirs(directory)
+        print("Directory {0} created".format(directory))
+    except Exception as error:
+        print("Error occured during creating directory: {0}".format(error))
+
+
+def create_repository(project_language, project_name, access_mode):
+    try:
+        user = Github(Credentials.username, Credentials.password).get_user()
+        repo = user.create_repo(name="{0}-{1}".format(project_language, project_name), private=access_mode)
+        print("Repository {0}-{1} created successfully, private set to: {2}".format(project_language, project_name, access_mode))
+    except Exception as error:
+        print("Error occured during deleting repository: {0}".format(error))
 
 
 if __name__ == "__main__":
